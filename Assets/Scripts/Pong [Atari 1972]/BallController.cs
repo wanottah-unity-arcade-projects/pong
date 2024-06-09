e7864592872b407b5808df3e7a16994b9953ac49
Wanottah <wanottah@gmail.com>
2024-06-09 15:51:10 +0100

    Initial commit

---
+++ Assets/Scripts/Pong [Atari 1972]/BallController.cs
@@ -0,0 +1,231 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+//
+// Pong [Atari 1972] v2019.02.24
+//
+// v2023.12.26
+//
+
+public class BallController : MonoBehaviour
+{
+    public static BallController ballController;
+
+    public Transform ballTransform;
+
+    public Rigidbody2D ballRigidbody;
+
+    // direction of ball
+    public float xBallVelocity;
+
+    public float yBallVelocity;
+
+    public float xBounceDirection;
+
+    public float yBounceDirection;
+
+    //public float yBounceOffset;
+
+    // speed of ball
+    public float ballSpeed;
+
+    // ball bounce speed
+    //public float ballBounceSpeed;
+
+    // ball speed increase
+    public float ballSpeedIncrease;
+
+    // maximum ball speed
+    public float maxBallSpeed;
+
+    // number of times ball hits the paddle
+    public int ballHitCounter;
+
+    // ball starting position
+    private float ballStartPositionX;
+
+    private float ballStartPositionY;
+
+    //private float paddleLength;
+
+    private float paddleCenter;
+
+    private float ballServeDelay;
+
+
+
+    private void Awake()
+    {
+        ballController = this;
+    }
+
+
+    public void Initialise()
+    {
+        // ball speed
+        ballSpeed = 2.5f;
+
+        // ball speed increase
+        ballSpeedIncrease = 0.1f;
+
+        // maximum ball speed
+        maxBallSpeed = 5f;
+
+        // number of times ball hits the paddle
+        ballHitCounter = 0;
+
+        ballStartPositionX = 0f;
+
+        ballStartPositionY = 0f;
+
+        // delays the ball before serving
+        ballServeDelay = 3f;
+
+        StartBall(ballSpeed, ballSpeed);      
+    }
+
+
+    // start moving the ball
+    public void StartBall(float xBallSpeed, float yBallSpeed)
+    {
+        // randomise player serve
+        float randomServeDirection = RandomServe(xBallSpeed);
+
+        float randomBallDirection = RandomDirection(yBallSpeed);
+
+        // activate the ball
+        ballTransform.gameObject.SetActive(true);
+
+        ballRigidbody.velocity = new Vector2(randomServeDirection, randomBallDirection) * (ballSpeed + ballSpeedIncrease * ballHitCounter);
+    }
+
+
+    // reset ball
+    public void ResetBall(float xVelocity, float yVelocity)
+    {
+        // reset ball hit counter
+        ballHitCounter = 0;
+
+        // reset ball position
+        ballTransform.position = new Vector2(ballStartPositionX, ballStartPositionY);
+
+        // start moving the ball after short delay
+        StartCoroutine(DelayBallServe(xVelocity, yVelocity));
+    }
+
+
+    private IEnumerator DelayBallServe(float ballSpeedX, float ballSpeedY)
+    {
+        yield return new WaitForSeconds(ballServeDelay);
+
+        StartBall(ballSpeedX, ballSpeedY);
+    }
+
+
+    public void FreezeBall()
+    {
+        xBallVelocity = ballRigidbody.velocity.x;
+
+        yBallVelocity = ballRigidbody.velocity.y;
+
+        ballRigidbody.constraints = RigidbodyConstraints2D.FreezePosition;
+    }
+
+
+    public void ResumeBall()
+    {
+        ballRigidbody.constraints = RigidbodyConstraints2D.None;
+
+        ballRigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+        ballRigidbody.velocity = new Vector2(xBallVelocity, yBallVelocity);
+    }
+
+
+    // change angle of ball when bouncing off paddle
+    public void PaddleBounce(Transform paddleTransform)
+    {
+        // increase ball hit counter
+        ballHitCounter++;
+
+        // determine which direction the ball should bounce
+        if (ballTransform.position.x > GameController.CENTRE_COURT)
+        {
+            xBounceDirection = -ballSpeed;
+        }
+
+        else
+        {
+            xBounceDirection = ballSpeed;
+        }
+
+        // calculate bounce angle
+        paddleCenter = paddleTransform.position.y;
+
+        yBounceDirection = (ballTransform.position.y - paddleCenter) * Player1Controller.player1.paddleLength;
+
+        // change angle and speed of ball
+        ballRigidbody.velocity = new Vector2(xBounceDirection, yBounceDirection) * (ballSpeed + ballSpeedIncrease * ballHitCounter);
+    }
+
+
+    // determines the serve direction of the ball
+    private float RandomServe(float ballSpeed)
+    {
+        float playerServe;
+
+        // if we are in attract mode or have just started a new game
+        //if (gameController.inDemoMode || (gameController.player1Score == 0 && gameController.player2Score == 0))
+        if (GameController.gameController.player1Score == 0 && GameController.gameController.player2Score == 0)
+        {
+            // randomise player serve
+            if (Random.Range(1, 100) > 40)
+            {
+                // player 1
+                playerServe = ballSpeed;
+
+                //gameController.SetBallColour(AtariConsoleController.atariConsoleController.tvMode, GameController.PLAYER_ONE);
+            }
+
+            else
+            {
+                // player 2
+                playerServe = -ballSpeed;
+
+                //gameController.SetBallColour(AtariConsoleController.atariConsoleController.tvMode, GameController.PLAYER_TWO);
+            }
+
+            // and return the player to serve
+            return playerServe;
+        }
+
+        // otherwise . . .
+        else
+        {
+            // just return the current ball direction
+            return ballSpeed;
+        }
+    }
+
+
+    // determines if the ball should move up or down
+    private float RandomDirection(float yVelocity)
+    {
+        float ballDirection;
+
+        if (Random.Range(1, 100) > 20)
+        {
+            ballDirection = -yVelocity;
+        }
+
+        else
+        {
+            ballDirection = yVelocity;
+        }
+
+        return ballDirection;
+    }
+
+
+} // end of class
---
+++ Assets/Scripts/Pong [Atari 1972]/CollisionController.cs
@@ -0,0 +1,62 @@
+﻿
+using UnityEngine;
+
+//
+// Pong [Atari 1972] v2019.02.24
+//
+// v2023.12.27
+//
+
+public class CollisionController : MonoBehaviour
+{
+    // check if the ball has entered the goal
+    private void OnTriggerEnter2D(Collider2D collidingObject)
+    {
+        if (!GameController.gameController.inAttractMode)
+        {
+            // disable the ball
+            BallController.ballController.ballTransform.gameObject.SetActive(false);
+
+            // play the 'goalScored' sound
+            AudioController.audioController.PlayAudioClip("Goal Scored");
+        }
+
+        // player 1 goal
+        if (collidingObject.CompareTag("Player 1 Goal"))
+        {
+            // update player 2 score
+            GameController.gameController.UpdateScore(GameController.PLAYER_TWO);
+
+            // reset ball position and speed
+            BallController.ballController.ResetBall(-BallController.ballController.ballSpeed, -BallController.ballController.ballSpeed);
+        }
+
+        // player 2 Goal
+        if (collidingObject.CompareTag("Player 2 Goal"))
+        {
+            // update player 1 score
+            GameController.gameController.UpdateScore(GameController.PLAYER_ONE);
+
+            // reset ball position and speed
+            BallController.ballController.ResetBall(BallController.ballController.ballSpeed, BallController.ballController.ballSpeed);
+        }
+    }
+
+
+    // when the ball bounces off the paddles
+    void OnCollisionEnter2D(Collision2D collidingObject)
+    {
+        if (!GameController.gameController.inAttractMode)
+        {
+            if (collidingObject.gameObject.CompareTag("Player 1") || collidingObject.gameObject.CompareTag("Player 2"))
+            {
+                // play a sound
+                AudioController.audioController.PlayAudioClip("Paddle Bounce");           
+
+                BallController.ballController.PaddleBounce(collidingObject.transform);
+            }
+        }
+    }
+
+
+} // end of class
